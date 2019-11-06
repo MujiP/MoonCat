f073bd8c3da2a312221b42397d2a133b0c56c5c2
Muji <MujiP@users.noreply.github.com>
2019-11-06 05:46:17 -0500

    Added my Bifrost framework (for networking) as an embedded binary.

---
+++ Bifrost.xcframework/ios-i386_x86_64-simulator/Bifrost.framework/Headers/Bifrost.h
@@ -0,0 +1,19 @@
+//
+//  Bifrost.h
+//  Bifrost
+//
+//  Created by Muji Paracha on 2018-08-20.
+//  Copyright © 2018 Muji Paracha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bifrost.
+FOUNDATION_EXPORT double BifrostVersionNumber;
+
+//! Project version string for Bifrost.
+FOUNDATION_EXPORT const unsigned char BifrostVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bifrost/PublicHeader.h>
+
+
