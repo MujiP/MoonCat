8d5502b882b9aafb3fef99b46c654441f4263b51
lur2 <39446669+lur2@users.noreply.github.com>
2019-11-29 15:59:57 -0500

    Initial setup.

---
+++ MoonCat/EventCreationPages_VC.swift
@@ -0,0 +1,41 @@
+//
+//  EventCreationView.swift
+//  MoonCat
+//
+//  Created by Kirk on 11/28/19.
+//  Copyright © 2019 Bolusaur. All rights reserved.
+//
+
+import UIKit
+
+class EventCreationView: UIView{
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        setupPagingView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Not using this")
+    }
+    
+    private func setupPagingView(){
+        
+    }
+    
+    private func setupTagEntryView(){
+        
+    }
+    
+    private func setupLocationCompletingView(){
+        
+    }
+    
+    private func setupTimeSlotView(){
+        
+    }
+    
+    private func setupPeopleView(){
+        
+    }
+    
+}
---
+++ MoonCat/EventCreation_VC.swift
@@ -0,0 +1,9 @@
+//
+//  EventCreation_VC.swift
+//  MoonCat
+//
+//  Created by Kirk on 11/28/19.
+//  Copyright © 2019 Bolusaur. All rights reserved.
+//
+
+import Foundation
