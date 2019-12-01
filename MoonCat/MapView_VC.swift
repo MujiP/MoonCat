a91fe74eb48b4be36b6287ae0c8049fb1e5d2edd
Si Yu(Viktor) Zhuang <siyu.zhuang@mail.utoronto.ca>
2019-12-01 01:30:45 -0500

    added functions to place an array of markers on map with sample markers

--- MoonCat/MapView_VC.swift
+++ MoonCat/MapView_VC.swift
@@ -15,6 +15,7 @@ class MapView_VC: UIViewController, CLLocationManagerDelegate {
     var mLocationManager = CLLocationManager()
     var mDidFindMyLocation = false
     let marker = GMSMarker()
+    var markers = [GMSMarker]()
     //var subView = UIView()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,11 +32,8 @@ class MapView_VC: UIViewController, CLLocationManagerDelegate {
         mapView.settings.zoomGestures = true
         mapView.settings.tiltGestures = true
         mapView.settings.rotateGestures = true
-        marker.title = "Tim Hortons"
-        marker.snippet = "Tims at SS"
-        marker.appearAnimation = .pop
-        marker.position = CLLocationCoordinate2D(latitude: 43.662279, longitude: -79.398489)
-        marker.map = mapView
+        createMarkerArray()
+        placeMarkers(markers: markers)
         self.view = mapView
     }
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus){
@@ -60,15 +58,48 @@ class MapView_VC: UIViewController, CLLocationManagerDelegate {
         }
     }
     // MARK: - Test placing markers from array
-    /*
+    
      
-     func createMarkerArray() -> {
-         let marker: GMSMarker = GMSMarker()
+     func createMarkerArray(){
+        let marker0: GMSMarker = GMSMarker()
+        let marker1: GMSMarker = GMSMarker()
+        let marker2: GMSMarker = GMSMarker()
+        let marker3: GMSMarker = GMSMarker()
+        let marker4: GMSMarker = GMSMarker()
+        
+        marker0.title = "Tim Hortons"
+        marker0.snippet = "Tims at SS"
+        marker0.appearAnimation = .pop
+        marker0.position = CLLocationCoordinate2D(latitude: 43.662279, longitude: -79.398489)
+        markers.append(marker0)
+        marker1.title = "Starbucks"
+        marker1.snippet = "Starbucks at MS"
+        marker1.appearAnimation = .pop
+        marker1.position = CLLocationCoordinate2D(latitude: 43.660866, longitude: -79.392577)
+        markers.append(marker1)
+        marker2.title = "Innis Cafe"
+        marker2.snippet = "Innis College Cafe"
+        marker2.appearAnimation = .pop
+        marker2.position = CLLocationCoordinate2D(latitude: 43.665600, longitude: -79.399540)
+        markers.append(marker2)
+        marker3.title = "Diabolos' Coffee"
+        marker3.snippet = "Campus hub for coffee & light eats"
+        marker3.appearAnimation = .pop
+        marker3.position = CLLocationCoordinate2D(latitude: 43.663311, longitude: -79.396418)
+        markers.append(marker3)
+        marker4.title = "McCafe"
+        marker4.snippet = "McCafe, great lattes"
+        marker4.appearAnimation = .pop
+        marker4.position = CLLocationCoordinate2D(latitude: 43.657924, longitude: -79.398682)
+        markers.append(marker4)
      }
-     func placeMarkers(){
-         
+    
+    func placeMarkers(markers: [GMSMarker]){
+        for marker in markers{
+            marker.map = mapView
+        }
      }
-     */
+     
     
 
 }
